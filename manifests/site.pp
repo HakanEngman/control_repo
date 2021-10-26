6eb510720c896f145c79a55c6a2cf9b9a0e47495
HakanEngman <69968183+HakanEngman@users.noreply.github.com>
2021-10-26 19:53:17 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,7 +5,8 @@ node 'master.puppet.vm' {
   include role::master_server
   file { '/root/README':
      ensure => file,
-     content => '$fqdn $kernel'
+     content => $fqdn 
+        $kernel,
      }
   }
 
