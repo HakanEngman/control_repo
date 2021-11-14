43b243b9646344246cc71fb22104d91e213a8fd5
HakanEngman <69968183+HakanEngman@users.noreply.github.com>
2021-11-14 20:35:36 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -17,8 +17,8 @@ class minecraft {
        ensure => file,
        source => 'puppet:///modules/minecraft/minecraft.service'
     }
-    service {'minecraft':
+    service { 'minecraft':
        ensure => running,
-       enable => true
+       enable => true,
        }
   }
