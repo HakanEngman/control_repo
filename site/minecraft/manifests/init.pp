b19b9734830316cedc269f96dd21bb3f62c007e2
HakanEngman <69968183+HakanEngman@users.noreply.github.com>
2021-11-14 20:46:42 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -13,7 +13,7 @@ class minecraft {
        ensure => file,
        content => 'eula=true'
     }
-    file {'/etc/sytemd/system/minecraft.service':
+    file {'/etc/systemd/system/minecraft.service':
        ensure => file,
        source => 'puppet:///modules/minecraft/minecraft.service'
     }
